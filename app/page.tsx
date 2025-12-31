6653cf5b6047c6d5de204846fa7fb32ed3f69906
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-31 16:53:24 +0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>
+            <p className="text-3xl text-indigo-500">coin pulcy</p>
+        </div>
+    )
+}
+export default Page
